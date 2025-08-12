9f2c99ef98c37321c3b39699245b7b53bf8c97a8
Bhagyasree <tellibhagyasree17@gmail.com>
2025-08-12 10:20:15 +0000

    its my 1st backend proect

--- server/app.js
+++ server/app.js
@@ -1,1 +1,10 @@
-console.log("hii its bhagyas");
+console.log("hii its bhagya");
+
+console.log("hello backend");
+
+let fname = "taj";
+let lname = "baba";
+console.log(fname + lname);
+
+let a = 10;
+console.log(a)
